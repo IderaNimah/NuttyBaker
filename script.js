f19428c0ec4205b9cb7c77b1cecb049e81e01dfc
IderaNimah <iderayusuf29@gmail.com>
2023-07-27 14:07:15 +0100

    discovery_page_

---
+++ script.js
@@ -0,0 +1,6 @@
+document
+  .getElementById("get_started_button")
+  .addEventListener("click", function () {
+    // Replace 'next-page.html' with the URL of your next page.
+    window.location.href = "Discovery.html";
+  });
