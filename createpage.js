fd830da01df995dd0d1780b3add7396b31eae058
IderaNimah <iderayusuf29@gmail.com>
2023-08-06 11:05:52 +0100

    awesome solve

--- createpage.js
+++ createpage.js
@@ -1,24 +1,46 @@
 "use strict";
-const passwordOne = document.getElementById('passwordOne');
-const passwordTwo = document.getElementById('passwordTwo');
-console.log(passwordOne);
+// const passwordOne = document.getElementById('passwordOne');
+// const passwordTwo = document.getElementById('passwordTwo');
+// console.log(passwordOne);
 
-const eyeIconOne = document.getElementById('eyeIconOne');
-const eyeIconTwo = document.getElementById('eyeIconTwo');
+// const eyeIconOne = document.getElementById('eyeIconOne');
+// const eyeIconTwo = document.getElementById('eyeIconTwo');
 
-function togglePasswordVisibility(){
-    const isTextType = passwordOne.type === 'text';
+// function togglePasswordVisibility(){
+//     const isTextType = passwordOne.type === 'text';
 
-    passwordOne.type = isTextType?'password':'text';
-    passwordTwo.type = isTextType?'password':'text';
+//     passwordOne.type = isTextType?'password':'text';
+//     passwordTwo.type = isTextType?'password':'text';
 
-    eyeIconOne.classList.toggle('fa-eye', isTextType);
-    eyeIconOne.classList.toggle('fa-eye-slash',!isTextType);
+//     eyeIconOne.classList.toggle('fa-eye', isTextType);
+//     eyeIconOne.classList.toggle('fa-eye-slash',!isTextType);
     
     
-    eyeIconTwo.classList.toggle('fa-eye', isTextType);
-    eyeIconTwo.classList.toggle('fa-eye-slash',!isTextType);
+//     eyeIconTwo.classList.toggle('fa-eye', isTextType);
+//     eyeIconTwo.classList.toggle('fa-eye-slash',!isTextType);
     
-}
-eyeIconOne.addEventListener('click', togglePasswordVisibility);
-eyeIconTwo.addEventListener('click', togglePasswordVisibility);
+// }
+// eyeIconOne.addEventListener('click', togglePasswordVisibility);
+// eyeIconTwo.addEventListener('click', togglePasswordVisibility);
+const eyeButtons = document.querySelectorAll("#eye");
+const eyeButtonOne = document.querySelector(".eyeOne");
+const eyeButtonTwo = document.querySelector(".eyeTwo");
+
+const passwordOne = document.querySelector("#passwordOne");
+const passwordTwo = document.querySelector("#passwordTwo");
+
+eyeButtons.forEach((eyeButton) => {
+	eyeButton.addEventListener("click", () => {
+		if (passwordOne.type === "password") {
+			passwordOne.type = "text";
+			passwordTwo.type = "text";
+			eyeButtonOne.innerHTML = '<i class="far fa-eye-slash"></i>';
+			eyeButtonTwo.innerHTML = '<i class="far fa-eye-slash"></i>';
+		} else {
+			passwordOne.type = "password";
+			passwordTwo.type = "password";
+			eyeButtonOne.innerHTML = '<i class="far fa-eye"></i>';
+			eyeButtonTwo.innerHTML = '<i class="far fa-eye"></i>';
+		}
+	});
+});
